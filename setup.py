f8c56b44b79acca51ae2b5ab2f03d33d1803fb05
Necdet Can Atesman <c.atesman@gmx.net>
2016-02-09 10:23:11 +0100

    Release: 0.2.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.webassets',
-    version='0.2',
+    version='0.2.1',
     description='Web assets management for The SCORE Framework',
     long_description=README,
     author='strg.at',
