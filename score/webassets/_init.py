fa6da9575eb6de8a300ddd6f4330b2f4c0099360
Necdet Can Ateşman <can@atesman.at>
2019-04-03 10:40:05 +0200

    Fix: Prevent exception if no bundle hash provided

--- score/webassets/_init.py
+++ score/webassets/_init.py
@@ -392,6 +392,9 @@ class ConfiguredWebassetsModule(ConfiguredModule):
             module, path = request.path.lstrip('/').split('/', maxsplit=1)
             if path.startswith('__bundle_') and path.endswith('__'):
                 def loader(hash_=None):
+                    if not hash_:
+                        raise AssetNotFound(module,
+                                            'bundle(%s)@None' % (name,))
                     name = path[len('__bundle_'):-2]
                     file = os.path.join(self.rootdir, module, name, hash_)
                     try:
