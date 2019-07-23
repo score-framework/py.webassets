07eea53b6ddd92e7143555a1aced9c7d60d4aa42
Necdet Can Ateşman <can@atesman.at>
2019-07-23 17:23:36 +0200

    New: Also add s-max-age header to bundles

--- score/webassets/_init.py
+++ score/webassets/_init.py
@@ -437,9 +437,10 @@ class ConfiguredWebassetsModule(ConfiguredModule):
                 mimetype, body = loader(hash_)
             except FileNotFoundError:
                 raise AssetNotFound(module, path)
+            max_age = 60 * 60 * 24 * 30 * 12
             return 200, {
                 'Content-Type': mimetype,
-                'Cache-Control': 'max-age=' + str(60 * 60 * 24 * 30 * 12),
+                'Cache-Control': 'max-age=%d s-max-age=%d' % (max_age, max_age)
                 'Etag': hash_,
                 'Last-Modified': email.utils.formatdate(),
             }, body
